c3054201d9d523306d4372f3a89732c33df8604d
nozari <dev.nozari@gmail.com>
2022-10-29 20:19:38 +0330

    create form validation for RegisterForm.js

--- fronted/src/components/login/RegisterForm.js
+++ fronted/src/components/login/RegisterForm.js
@@ -1,29 +1,83 @@
 import { Form, Formik } from "formik";
 import RegisterInput from "../inputs/registerInput";
 import { useState } from "react";
-
-const userInfos = {
-  first_name: "",
-  last_name: "",
-  email: "",
-  password: "",
-  bYear: "",
-  bMonth: "",
-  bDay: "",
-  gender: "",
-};
-
+import * as Yup from "yup";
 export default function RegisterForm() {
+  const userInfos = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth() + 1,
+    bDay: new Date().getDate(),
+    gender: "",
+  };
+
   const [user, setUser] = useState(userInfos);
+
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = user;
+
+  const years = Array.from(new Array(100), (val, index) => bYear - index);
+  const months = Array.from(new Array(12), (val, index) => 1 + index);
+  const getDays = () => new Date(bYear, bMonth, 0).getDate();
+  const days = Array.from(new Array(getDays()), (val, index) => 1 + index);
 
   const handlRegisterChange = (event) => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
   };
 
+  const registervalidation = Yup.object({
+    first_name: Yup.string()
+      .required("First Name is required.")
+      .min(2, "First Name must be at least 2 characters")
+      .max(30, "First Name must be at most 30 characters")
+      .matches(
+        /^[aA-zZ]+$/,
+        "First Name and special characters are not allowed."
+      ),
+    last_name: Yup.string()
+      .required("Last Name is required.")
+      .min(2, "Last Name must be at least 2 characters")
+      .max(30, "Last Name must be at most 30 characters")
+      .matches(
+        /^[aA-zZ]+$/,
+        "Last Name and special characters are not allowed."
+      ),
+    email: Yup.string()
+      .required("Email is required.")
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required("Passwrod is required.")
+      .min(6, "Passwrod must be at least 2 characters"),
+  });
+
   return (
     <div className="login_wrapper_wrap_body_form">
-      <Formik>
+      <Formik
+        enableReinitialize
+        initialValues={{
+          first_name,
+          last_name,
+          email,
+          password,
+          bYear,
+          bMonth,
+          bDay,
+          gender,
+        }}
+        validationSchema={registervalidation}
+      >
         {(formik) => (
           <Form className="login_wrapper_wrap_body_form_register">
             <div className="login_wrapper_wrap_body_form_register_line">
@@ -56,14 +110,34 @@ export default function RegisterForm() {
                   Date of birth <div className="info_icon"></div>
                 </div>
                 <div className="login_wrapper_wrap_body_form_register_line_col_body">
-                  <select name="bDay">
-                    <option>1</option>
+                  <select
+                    name="bDay"
+                    value={bDay}
+                    onChange={handlRegisterChange}
+                  >
+                    {days.map((item) => (
+                      <option key={item} value={item}>
+                        {item}
+                      </option>
+                    ))}
                   </select>
-                  <select name="bMonth">
-                    <option>1</option>
+                  <select
+                    name="bMonth"
+                    value={bMonth}
+                    onChange={handlRegisterChange}
+                  >
+                    {months.map((item) => (
+                      <option key={item} value={item}>
+                        {item}
+                      </option>
+                    ))}
                   </select>
-                  <select name="bYear">
-                    <option>1993</option>
+                  <select name="bYear" onChange={handlRegisterChange}>
+                    {years.map((item) => (
+                      <option key={item} value={item}>
+                        {item}
+                      </option>
+                    ))}
                   </select>
                 </div>
               </div>
