6a58f33bcf4faf0156d46053c812935014a98fcf
nozari <dev.nozari@gmail.com>
2022-11-16 20:58:13 +0330

    Create Emoji for PostCreatePopup

--- fronted/src/components/create-post-popup/CreatePostPopup.js
+++ fronted/src/components/create-post-popup/CreatePostPopup.js
@@ -1,9 +1,28 @@
 import "./CreatePostPopup.scss";
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Picker from "emoji-picker-react";
+import { useRef } from "react";
 export default function CreatePostPopup() {
   const { user } = useSelector((state) => ({ ...state }));
   const [text, setText] = useState("");
+  const [picker, setPicker] = useState(false);
+  const textRef = useRef(null);
+  const [cursorPosition, setCursorPosition] = useState();
+
+  useEffect(() => {
+    textRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
+
+  const handleEmoji = ({ emoji }) => {
+    const ref = textRef.current;
+    ref.focus();
+    const start = text.substring(0, ref.selectionStart);
+    const end = text.substring(ref.selectionStart);
+    const newText = start + emoji + end;
+    setText(newText);
+    setCursorPosition(start.length + emoji.length);
+  };
   return (
     <div className="blur">
       <div className="post__box">
@@ -35,13 +54,29 @@ export default function CreatePostPopup() {
           </div>
           <div className="post__box__body__text">
             <textarea
+              ref={textRef}
               maxLength={100}
               value={text}
               placeholder={` What's on your mind,${user.first_name}?`}
               onChange={(e) => setText(e.target.value)}
             ></textarea>
           </div>
-          <div className="post__box__body__color"></div>
+          <div className="post__box__body__emoji">
+            <div className="post__box__body__emoji__color">
+              <img src="../../../icons/colorful.png" alt="" />
+            </div>
+            <div className="post__box__body__emoji__btn">
+              <i
+                className="emoji_icon_large"
+                onClick={() => {
+                  setPicker((prev) => !prev);
+                }}
+              ></i>
+              <div className="post__box__body__emoji__btn__menu">
+                {picker && <Picker onEmojiClick={handleEmoji} />}
+              </div>
+            </div>
+          </div>
         </div>
         <div className="post__box__footer">
           <div className="post__box__footer__post"></div>
